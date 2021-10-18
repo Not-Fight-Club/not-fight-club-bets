df2004fefd0d8c8d4ce6dcd005e7785575b038a7
BlakeDrostRevature <blake.drost@revature.net>
2021-10-18 16:41:02 -0500

    Added XML Comments

--- BetsApi_Business/Repository/WagerRepo.cs
+++ BetsApi_Business/Repository/WagerRepo.cs
@@ -17,6 +17,11 @@ namespace BetsApi_Business.Repository {
         public WagerRepo(WageDbContext context) {
             _context = context;
         }
+        /// <summary>
+        /// Purpose: Return a converted Entity Framework Model to a View Model.
+        /// </summary>
+        /// <param name="ef"></param>
+        /// <returns>ViewWager</returns>
         public ViewWager EFToView(Wager ef) {
             ViewWager vw = new ViewWager();
             vw.UserId = ef.UserId;
@@ -25,7 +30,11 @@ namespace BetsApi_Business.Repository {
             vw.Amount = ef.Amount;
             return vw;
         }
-
+        /// <summary>
+        /// Purpose: Return a View Model to an Entity Framework Model by searching the Database.
+        /// </summary>
+        /// <param name="view"></param>
+        /// <returns>Wager</returns>
         public async Task<Wager> ViewToEF(ViewWager view) {           
             Wager wm = new Wager();
 
@@ -43,7 +52,10 @@ namespace BetsApi_Business.Repository {
             }
             return wm;
         }
-
+        /// <summary>
+        /// Purpose: Return all records from the Wager Table as a list of View Models.
+        /// </summary>
+        /// <returns>List of ViewWager</returns>
         public async Task<List<ViewWager>> WagerListAsnyc() {
             ViewWager vws = new ViewWager();
             List<ViewWager> vwsl = new List<ViewWager>();
@@ -61,7 +73,11 @@ namespace BetsApi_Business.Repository {
             }
             return vwsl; 
         }
-
+        /// <summary>
+        /// Purpose: Return all wager records for a specific fight
+        /// </summary>
+        /// <param name="curFightId"></param>
+        /// <returns>List of ViewWager</returns>
         public async Task<List<ViewWager>> SpecificWagerListAsnyc(int curFightId) {
             ViewWager vws = new ViewWager();
             List<ViewWager> vwsl = new List<ViewWager>();
@@ -80,7 +96,12 @@ namespace BetsApi_Business.Repository {
             }
             return vwsl;
         }
-
+        /// <summary>
+        /// Purpose: Calculates the winning bets for each user that bet on the correct fighter
+        /// </summary>
+        /// <param name="curFightId"></param>
+        /// <param name="winningFighterId"></param>
+        /// <returns>List of ViewUser</returns>
         public async Task<List<ViewUser>> ReturnUsersToPayoutsAsnyc(int curFightId, int winningFighterId)
         {
             //List of Winners
@@ -93,7 +114,7 @@ namespace BetsApi_Business.Repository {
                 where o.FighterId != winningFighterId && o.FightId == curFightId
                 select new { o.UserId, o.Amount }
                 ).ToListAsync();
-            //Tptal Won and Lost by Winners and Losers
+            //Total Won and Lost by Winners and Losers
             double totalWinningBets = winnerBets.Select( c =>  c.Amount).Sum();
             double totalLosingBets =  loserBets.Select( c =>  c.Amount).Sum();
             
@@ -116,20 +137,25 @@ namespace BetsApi_Business.Repository {
             }            
             return userToBePaid;
         }
-
+        /// <summary>
+        /// Purpose: Updates a wager record
+        /// </summary>
+        /// <param name="vw"></param>
+        /// <returns>ViewWager</returns>
         public async Task<ViewWager> putWagerAsnyc(ViewWager vw)
-        {
-        
+        {        
             var updatedWager = await _context.Wagers.Where(e => e.UserId == vw.UserId && e.FightId == vw.FightId && e.FighterId == vw.FighterId).FirstOrDefaultAsync();
             updatedWager.Amount = vw.Amount; 
             _context.Update(updatedWager);
          
            await _context.SaveChangesAsync();
-           return vw;
-           
+           return vw;          
         }
-
-
+        /// <summary>
+        /// Purpose: Places a wager record into the Wager Table
+        /// </summary>
+        /// <param name="vw"></param>
+        /// <returns>ViewWager</returns>
         public async Task<ViewWager> PostWagerAsync(ViewWager vw)
         {
             var wagerToPost = new Wager() {
